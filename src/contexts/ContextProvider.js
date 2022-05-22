594f9c8a915a5729512668849a7db2a019b7a822
Azathot11 <87489449+Azathot11@users.noreply.github.com>
2022-05-22 05:11:46 +0100

    my dashboard

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,42 @@
+import React,{createContext,useContext,useState} from "react";
+
+const StateContext = createContext();
+
+const initialState ={
+    chat:false,
+    cart:false,
+    userProfile:false,
+    notification:false
+}
+
+export const ContextProvider=(props)=>{
+    const [activeMenu,setActiveMenu] = useState(true);
+    const [isClicked,setIsClicked] = useState(initialState);
+    const[screenSize,setScreenSize] = useState(undefined)
+    
+   const handleClick=(clicked)=>{
+       setIsClicked({...initialState, [clicked]:true});
+   }
+       
+   
+
+
+
+    return (
+      <StateContext.Provider
+        value={{
+          activeMenu,
+         setActiveMenu,
+         isClicked,
+         setIsClicked,
+         handleClick,
+         screenSize,
+         setScreenSize
+        }}
+      >
+        {props.children}
+      </StateContext.Provider>
+    );
+}
+
+export const useStateContext = ()=> useContext(StateContext);
